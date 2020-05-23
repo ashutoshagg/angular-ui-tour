22b6966ab1a4838fc60d51156bfd1ab2ea4a8b83
Ashutosh Aggarwal <ashutosh.aggarwal4@gmail.com>
2020-05-23 06:28:25 +0530

    ddsf

--- src/main.ts
+++ src/main.ts
@@ -88,8 +88,3 @@ export class DemoMaterialModule { }
 
 
 platformBrowserDynamic().bootstrapModule(AppModule);
-
-
-/**  Copyright 2018 Google Inc. All Rights Reserved.
-    Use of this source code is governed by an MIT-style license that
-    can be found in the LICENSE file at http://angular.io/license */
